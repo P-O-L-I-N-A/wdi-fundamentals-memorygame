789df19e86c1a11856828190a126cb3a4b5977e6
P-O-L-I-N-A <Polina.vo@gmail.com>
2018-10-11 15:21:44 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,30 @@
 console.log("User flipped " + "queen");
 console.log("User flipped " + "king");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+//cardsInPlay could b subed 4 cardOne
+console.log('User flipped ' + cardsInPlay[0]);
+console.log('User flipped ' + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2);
+if(cardsInPlay[0] === cardsInPlay[1]) { 
+  alert("You found a match!");
+} else if (alert("Sorry, try again.")){
+}
+
+
+ 
+
+
+
+
+
+
+
+
